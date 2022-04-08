21e8c3683d5be3191c893ca9ffb44c9d1d0f5abd
Wison Ye <wisonye@gmail.com>
2022-04-09 10:51:57 +1200

    [ master ] - reforamtted with new settings.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 //
 // How to compile and run this program?
 //
-// clang++ -Wall -std=c++17 -stdlib=libc++ main.cpp -o test && ./test
+// clang++ -Wall -std=c++17 -stdlib=libc++ main.cpp -o test
+// && ./test
 //
 #include <cstdint>
 #include <iostream>
@@ -11,7 +12,9 @@ using namespace std;
 //
 //
 //
-int add_together(int number_one, int number_2) { return number_one + number_2; }
+int add_together(int number_one, int number_2) {
+    return number_one + number_2;
+}
 
 //
 //
@@ -22,30 +25,30 @@ void show_info(char *name) {}
 //
 //
 int main() {
-  cout << "Hi, I'm Fion:)" << endl;
+    cout << "Hi, I'm Fion:)" << endl;
 
-  uint8_t my_version = 0xFF;
-  bool is_success = true;
+    uint8_t my_version = 0xFF;
+    bool is_success = true;
 
-  if (is_success) {
-    cout << "'is_success' is equal to 'true'!!!\n\n";
-  }
+    if (is_success) {
+        cout << "'is_success' is equal to 'true'!!!\n\n";
+    }
 
-  show_info((char *)"asfasdf");
+    show_info((char *)"asfasdf");
 
-  int add_result = add_together(45, -99);
-  cout << add_result << "\n\n";
+    int add_result = add_together(45, -99);
+    cout << add_result << "\n\n";
 
-  //
-  // C++14 feature
-  //
-  auto return_value_itself = [](auto x) { return x; };
+    //
+    // C++14 feature
+    //
+    auto return_value_itself = [](auto x) { return x; };
 
-  int int_value = return_value_itself(888);
-  string string_value = return_value_itself("Wison Ye");
+    int int_value = return_value_itself(888);
+    string string_value = return_value_itself("Wison Ye");
 
-  cout << "int_value :" << int_value << "\n";
-  cout << "string_value :" << string_value << "\n";
+    cout << "int_value :" << int_value << "\n";
+    cout << "string_value :" << string_value << "\n";
 
-  return 0;
+    return 0;
 }
